b77f510952e2eed919bd7f044d940afee00bcbff
Andrew Li <andrewli@u.northwestern.edu>
2026-02-17 01:58:38 +0000

    add a logger

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from measurement.common import print_project_name
+from pathlib import Path
+
+import arrow
+
+from measurement.lake.measurement_lake_controller import MeasurementLakeController
 
 if __name__ == "__main__":
-    print_project_name()
+    lake = MeasurementLakeController.create(Path("/mnt/apple/cisco-umbrella-2019-2025"))
+
+    lake.download(arrow.get("2025-02-01"), arrow.get("2025-02-11"))
